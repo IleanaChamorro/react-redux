ae27d8c1b94c4851ab9b1a0c5bca082be5e8c299
Ileana <ic.chamorro07@gmail.com>
2023-05-24 15:23:42 -0300

    REDUX Estructura de archivos

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+//Centralizado de reducers
+//funciones que reciben el estado actual y la acción a realizar y devuelven un nuevo estado, sin modificar directamente el estado actual
+
+import { combineReducers } from "redux";
+
+const reducer = combineReducers({});
+
+export default reducer;
